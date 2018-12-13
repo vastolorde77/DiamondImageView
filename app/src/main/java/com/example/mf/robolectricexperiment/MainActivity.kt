5dec86098df88a911321ef152a1e49ca4acbadf0
MF <vastolorde77@gmail.com>
2018-12-13 15:38:45 +0700

    diamond image view init

--- app/src/main/java/com/example/mf/robolectricexperiment/MainActivity.kt
+++ app/src/main/java/com/example/mf/robolectricexperiment/MainActivity.kt
@@ -10,36 +10,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
-    private val TAG = MainActivity::class.simpleName
-    private val notificationHelper by lazy{NotificationHelper(this)}
-    private val NOTI = mapOf<String,Int>("PRI1" to 1100,"PRI2" to 1101,"SECOND1" to 1200, "SECOND2" to 1201)
-
-    companion object {
-        val JOB_ID = 0
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setOnClicks()
-        Glide.with(this).load("https://picsum.photos/${Random.nextInt(200,500)}").into(imageView)
 
     }
-
-    override fun onResume() {
-        super.onResume()
-
-    }
-
-    private fun setOnClicks() {
-        schedule.setOnClickListener {
-
-        }
-        cancel.setOnClickListener {
-//            imageView.toggleRound()
-        }
-
-    }
-
 
 }
